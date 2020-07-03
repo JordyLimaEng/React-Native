93eb59fb5afafbd9fc7bff7cbf2d11fad120c194
JordyLimaEng <jordy.sousa@eng.ci.ufpb.br>
2020-07-03 19:52:17 -0300

    Botão e diferentes chamadas de função

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import React from 'react'
 import { View, StyleSheet } from 'react-native'
 
-import Titulo from './components/Titulo'
-// import Aleatorio from './components/aleatorio'
-// import MinMax from './components/MinMax'
-// import CompPadrao, {Comp1,Comp2} from './components/Multi'
-// import Primeiro from './components/Primeiro'
+import Botao from './components/Botao'
 
 export default () =>  (
 
     <View style={style.App}>
-        <Titulo principal = "Cadastro Produto"
-                secundario = "Tela de Cadastro do produto" />
+        <Botao />
     </View>
 
 )
---
+++ src/components/Botao.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import {Button} from 'react-native'
+
+export default props =>{
+    
+    function executar(){
+        console.warn("#1")
+    }
+
+    return(
+        <>
+            <Button 
+            title="Exec1"
+            onPress = {executar}
+            />
+            <Button 
+            title="Exec2"
+            onPress = {function(){console.warn("#2")}}
+            />
+            <Button 
+            title="Exec3"
+            onPress = {()=>console.warn("#3")}
+            />
+        </>
+    )
+}
