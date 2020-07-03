29dad94fb50666132118d36b6f24997a7349b672
JordyLimaEng <jordy.sousa@eng.ci.ufpb.br>
2020-07-03 19:41:06 -0300

    Envolvendo elementos com fragment em tags

--- src/components/Titulo.js
+++ src/components/Titulo.js
@@ -3,8 +3,8 @@ import {View,Text} from 'react-native'
 import Estilo from './estilo'
 
 export default props =>(
-        <React.Fragment>
+        <>
             <Text style={Estilo.fontG}> {props.principal}</Text>
             <Text style={Estilo.fontP}> {props.secundario}</Text>
-        </React.Fragment>
+        </>
 )
