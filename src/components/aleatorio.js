3e63f1acc48212ad83ce8fc3d3cdfa41e2423ae7
JordyLimaEng <jordy.sousa@eng.ci.ufpb.br>
2020-07-03 19:15:09 -0300

    Manipulando props usando 'des'tructing

--- src/components/aleatorio.js
+++ src/components/aleatorio.js
@@ -2,11 +2,14 @@ import React from 'react'
 import {Text} from 'react-native'
 import Estilo from './estilo'
 
-export default(props) => 
+export default(props) => {
+        const {max, min} = props
+        return (
         <Text style={Estilo.fontP}>
-            Max = {props.max}, Min = {props.min}, Aleatorio = {Random(props.max,props.min)}
+            Max = {max}, Min = {min}, Aleatorio = {Random(max,min)}
         </Text>
-
+        )
+}
 function Random(max, min){   
     let d = max - min + 1
     return Math.floor(Math.random() * d) + min
